4d69b9f0d628be9c59c453ee95511d39ec002820
Jim Ray <jimiray@gmail.com>
2014-05-27 15:00:00 -0600

    updates for generators

--- lib/generators/gorilla_test/install/gorilla_test_generator.rb
+++ lib/generators/gorilla_test/install/gorilla_test_generator.rb
@@ -2,7 +2,7 @@ module GorillaTest
   module Generators
 
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path("../templates", __FILE__)
       desc "Creates GorillaTest initializer for your application"
       def copy_initializer
         template 'gorilla_test.rb', 'config/initializers/gorilla_test.rb'
