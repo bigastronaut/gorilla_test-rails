957a5663c6016036ce3fbe06492b02041ccfecac
Jim Ray <jimiray@gmail.com>
2014-05-27 14:45:57 -0600

    fix for class name

--- lib/generators/gorilla_test_generator.rb
+++ lib/generators/gorilla_test_generator.rb
@@ -1,9 +1,9 @@
 module GorillaTest
   module Generators
 
-    class Install < Rails::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
-      desc "Creates Jem initializer for your application"
+      desc "Creates GorillaTest initializer for your application"
       def copy_initializer
         template 'gorilla_test.rb', 'config/initializers/gorilla_test.rb'
       end
