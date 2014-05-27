b954a65ded3723c0e6090ee5fc4766b1673c969f
Jim Ray <jimiray@gmail.com>
2014-05-27 15:33:21 -0600

    update configuration and update generator template to use env vars by default

--- lib/generators/gorilla_test/install/templates/gorilla_test.rb
+++ lib/generators/gorilla_test/install/templates/gorilla_test.rb
@@ -1,5 +1,5 @@
 GorillaTest.configure do |config|
   # Set this options to what makes sense for you
-  config.api_key = 'your api key'
-  config.project_id = 'your project id'
+  config.api_key = ENV['GORILLA_TEST_API_KEY']
+  config.project_id = ENV['GORILLA_TEST_PROJECT_ID']
 end
--- lib/gorilla_test.rb
+++ lib/gorilla_test.rb
@@ -9,18 +9,46 @@ module GorillaTest
     yield(config)
   end
 
-  def self.with_configuration(config)
-    original_config = {}
+  # def self.with_configuration(config)
+  #   original_config = {}
 
-    config.each do |key, value|
-      original_config[key] = configuration.send(key)
-      configuration.send("#{key}=", value)
+  #   config.each do |key, value|
+  #     original_config[key] = configuration.send(key)
+  #     configuration.send("#{key}=", value)
+  #   end
+
+  #   result = yield if block_given?
+
+  #   original_config.each { |key, value| configuration.send("#{key}=", value) }
+  #   result
+  # end
+
+  class Configuration
+
+    GORILLA_TEST_API_URL = "https://www.gorillatest.com/api"
+    GORILLA_TEST_NON_SSL_API_URL = "http://www.gorillatest.com/api"
+    DEFAULT_ENV = 'production'
+
+    attr_accessor :ssl_api_url,
+                  :nonssl_api_url,
+                  :api_key,
+                  :project_id,
+                  :env
+
+    def initialize #:nodoc:
+      @ssl_api_url    = GORILLA_TEST_API_URL
+      @nonssl_api_url = GORILLA_TEST_NON_SSL_API_URL
+      @api_key        = ENV['GORILLA_TEST_API_KEY']
+      @project_id     = ENV['GORILLA_TEST_PROJECT_ID']
+      @env            = DEFAULT_ENV
     end
 
-    result = yield if block_given?
+    def api_server_url(ssl = nil) #:nodoc:
+      ssl = use_ssl_by_default if ssl.nil?
+      ssl ? ssl_api_url : nonssl_api_url
+    end
 
-    original_config.each { |key, value| configuration.send("#{key}=", value) }
-    result
   end
+
 
 end
