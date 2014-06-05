6689376c2fc395536c7dba78fd4e4f713cb54728
Jim Ray <jimiray@gmail.com>
2014-06-05 10:10:11 -0600

    update gorilla test heroku keys

--- lib/generators/gorilla_test/install/templates/gorilla_test.rb
+++ lib/generators/gorilla_test/install/templates/gorilla_test.rb
@@ -1,5 +1,5 @@
 GorillaTest.configure do |config|
   # Set this options to what makes sense for you
-  config.api_key = ENV['GORILLA_TEST_API_KEY']
-  config.project_id = ENV['GORILLA_TEST_PROJECT_ID']
+  config.api_key = ENV['GORILLATEST_API_KEY']
+  config.project_id = ENV['GORILLATEST_PROJECT_ID']
 end
